403258a62794cd33fc799a25e5ff852d108bbc40
unchaptered <workstation19961002@gmail.com>
2022-11-14 19:24:37 +0900

    Initial commit

---
+++ test/e-commerce-be.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ECommerceBe from '../lib/e-commerce-be-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/e-commerce-be-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ECommerceBe.ECommerceBeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
