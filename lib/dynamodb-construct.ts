4ce3ea8f1e820468b5ca9daf39a38d0aba4e2b88
unchaptered <workstation19961002@gmail.com>
2022-11-26 12:46:27 +0900

    Update(CDK-Stack) DynamoDBConstruct.prototype.basketTable

--- lib/dynamodb-construct.ts
+++ lib/dynamodb-construct.ts
@@ -8,15 +8,29 @@ import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 export class DynamoDBConstruct extends Construct {
 
     public readonly productTable: Table;
+    public readonly basketTable: Table;
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
+        // product : PK: id -- name - description - imageFile - price - category
         this.productTable = new Table(this, 'product', {
             partitionKey: {
                 name: 'id',
                 type: AttributeType.STRING
             },
             tableName: 'product',
+            removalPolicy: RemovalPolicy.DESTROY,
+            billingMode: BillingMode.PAY_PER_REQUEST
+        });
+
+        // basket : PK: id - username - basektItemList[]
+        // basketItem : { quantity - color - price - productId - productName }
+        this.basketTable = new Table(this, 'basket', {
+            partitionKey: {
+                name: 'username',
+                type: AttributeType.STRING
+            },
+            tableName: 'basket',
             removalPolicy: RemovalPolicy.DESTROY,
             billingMode: BillingMode.PAY_PER_REQUEST
         });
